d7dd5b7b2b1d93e9c62f0fea6e76858f6568ea32
Philip William <152656767+LittleOTwo@users.noreply.github.com>
2026-07-10 21:35:06 -0300

    Poli

--- scrapers/items.py
+++ scrapers/items.py
@@ -5,8 +5,7 @@
 
 import scrapy
 
-
-class ScrapersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ChatbotContentItem(scrapy.Item):
+    url = scrapy.Field()
+    titulo = scrapy.Field()
+    texto_limpo = scrapy.Field()
