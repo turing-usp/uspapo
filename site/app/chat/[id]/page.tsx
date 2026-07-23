d566ab1b29c85031f66f06d4167eb2e2aa245ed4
Luca Henrique <lucahenriquetavares@gmail.com>
2026-07-23 19:57:01 -0300

    Adição do arquivo .env

--- site/app/chat/[id]/page.tsx
+++ site/app/chat/[id]/page.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { useParams } from "next/navigation";
 import { useEffect, useRef } from "react";
 
+
 export default function ChatPage() {
 
     const { id } = useParams();
@@ -17,7 +18,7 @@ export default function ChatPage() {
 
     const enviarParaAPI = async (texto: string) => {
     try {
-        const res = await fetch("http://localhost:5000/chat", {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/chat`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ pergunta: texto }),
